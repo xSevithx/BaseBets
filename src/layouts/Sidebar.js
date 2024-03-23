750d420f94b22633da93f69427c868d97e9ab27e
xSevithx <sevithstarr@gmail.com>
2024-03-23 13:46:47 -0500

    Refactor Sidebar component to include collapsible sub-navigation

--- src/layouts/Sidebar.js
+++ src/layouts/Sidebar.js
@@ -1,5 +1,6 @@
-import { Button, Nav, NavItem } from "reactstrap";
-import { Link, useLocation } from "react-router-dom";
+import React, { useState } from 'react';
+import { Button, Nav, NavItem, Collapse } from 'reactstrap';
+import { Link, useLocation } from 'react-router-dom';
 
 const navigation = [
 
@@ -29,9 +30,12 @@ const Sidebar = () => {
   const showMobilemenu = () => {
     document.getElementById("sidebarArea").classList.toggle("showSidebar");
   };
+  const [isOpen, setIsOpen] = useState(null);
+  const toggle = (index) => setIsOpen(isOpen === index ? null : index);
   let location = useLocation();
 
   return (
+    <>
     <div className="bg-dark">
       <div className="d-flex">
         <Button
@@ -62,8 +66,44 @@ const Sidebar = () => {
           ))}
         </Nav>
       </div>
-      <a href="/">Root</a>
     </div>
+
+
+    <div className="bg-dark">
+      <div className="d-flex">
+        <Button
+          color="white"
+          className="ms-auto text-white d-lg-none"
+          onClick={() => document.getElementById("sidebarArea").classList.toggle("showSidebar")}
+        >
+          <i className="bi bi-x"></i>
+        </Button>
+      </div>
+      <div className="p-3 mt-2">
+        <Nav vertical className="sidebarNav">
+          {navigation.map((navi, index) => (
+            <NavItem key={index} className="sidenav-bg">
+              <Link
+                to="#"
+                className={
+                  location.pathname === navi.href
+                    ? "active nav-link py-3"
+                    : "nav-link py-3"
+                }
+                onClick={() => toggle(index)}
+              >
+                <i className={navi.icon}></i>
+                <span className="ms-3 d-inline-block">{navi.title}</span>
+              </Link>
+              <Collapse isOpen={isOpen === index}>
+                {/* Render sub-navigation items here if any */}
+              </Collapse>
+            </NavItem>
+          ))}
+        </Nav>
+      </div>
+    </div>
+    </>
   );
 };
 
