b41294b482a7c4f414b9d34744ce0d85fea956f3
xSevithx <sevithstarr@gmail.com>
2024-04-20 10:29:58 -0500

    Update logo component in Logo.js

--- src/layouts/Logo.js
+++ src/layouts/Logo.js
@@ -1,11 +1,11 @@
 // Logo component
 import React from 'react';
-const logo = require("../assets/images/logos/BaseBetsLogo.png");
+const logo = require("../assets/images/logos/20side.png");
 
 const Logo = () => {
   return (
     <div style={{ position: 'absolute', top: 0, left: 0, padding: '10px' }}>
-      <img height='50em' src={logo} alt="Block Bets Logo" />
+      <img height='45em' src={logo} alt="Block Bets Logo" />
     </div>
   );
 };
