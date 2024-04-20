528f2a2b2d5d8f66461d952149b45079f2c1649c
xSevithx <sevithstarr@gmail.com>
2024-04-20 10:35:32 -0500

    Update WebSocket URL in BetsContext.js

--- src/contexts/BetsContext.js
+++ src/contexts/BetsContext.js
@@ -6,7 +6,7 @@ export const BetsProvider = ({ children }) => {
     const [latestBet, setLatestBet] = useState(null);
 
     useEffect(() => {
-        const ws = new WebSocket('ws://127.0.0.1:8080');
+        const ws = new WebSocket('ws://206.189.59.181:8080');
 
         ws.onmessage = (event) => {
             if (event.data instanceof Blob) {
