e73b51f93f67fd1779b0e3e17bc427188b6ffaff
xSevithx <sevithstarr@gmail.com>
2024-04-19 22:28:24 -0500

    Update background styles and card layout in CardStyle.css and Starter.js

--- src/views/Starter.js
+++ src/views/Starter.js
@@ -18,122 +18,8 @@ import 'reactjs-popup/dist/index.css';
 import { toast } from 'react-toastify'; // Assuming you're using react-toastify
 import 'react-toastify/dist/ReactToastify.css';
 
-
-const BlogData = [
-  {
-    image: bg1,
-    title: "This is simple blog",
-    subtitle: "2 comments, 1 Like",
-    description:
-      "This is a wider card with supporting text below as a natural lead-in to additional content.",
-    btnbg: "primary",
-  },
-  {
-    image: bg2,
-    title: "Lets be simple blog",
-    subtitle: "2 comments, 1 Like",
-    description:
-      "This is a wider card with supporting text below as a natural lead-in to additional content.",
-    btnbg: "primary",
-  },
-  {
-    image: bg3,
-    title: "Don't Lamp blog",
-    subtitle: "2 comments, 1 Like",
-    description:
-      "This is a wider card with supporting text below as a natural lead-in to additional content.",
-    btnbg: "primary",
-  },
-  {
-    image: bg4,
-    title: "Simple is beautiful",
-    subtitle: "2 comments, 1 Like",
-    description:
-      "This is a wider card with supporting text below as a natural lead-in to additional content.",
-    btnbg: "primary",
-  },
-];
-
-const PopupExample = ({ shortenAddress }) => {
-  // Define your style variables
-  const fontSize = '12px';
-  const padding = '5px';
-  const backgroundColor = '#ffffff';
-  const borderColor = '#cfcece';
-  
-  // Define inline styles using the variables
-  const modalStyle = {
-    fontSize: fontSize,
-    padding: padding,
-    backgroundColor: backgroundColor,
-    border: `1px solid ${borderColor}`,
-  };
-  
-  const headerStyle = {
-    width: '100%',
-    borderBottom: `1px solid ${borderColor}`,
-    fontSize: '18px',
-    textAlign: 'center',
-    padding: '5px',
-  };
-  
-  const contentStyle = {
-    width: '100%',
-    padding: '10px 5px',
-  };
-  
-  const actionsStyle = {
-    width: '100%',
-    padding: '10px 5px',
-    margin: 'auto',
-    textAlign: 'center',
-  };
-  
-  const closeStyle = {
-    cursor: 'pointer',
-    position: 'absolute',
-    display: 'block',
-    padding: '2px 5px',
-    lineHeight: '20px',
-    right: '-10px',
-    top: '-10px',
-    fontSize: '24px',
-    backgroundColor: backgroundColor,
-    borderRadius: '18px',
-    border: `1px solid ${borderColor}`,
-  };
-
-  return (
-    <>
-    <Popup trigger={<button className="button"> Open Modal </button>} modal nested>
-      {close => (
-        <div style={modalStyle}>
-          <button style={closeStyle} onClick={close}>
-            &times;
-          </button>
-          <div style={headerStyle}> Modal Title {shortenAddress} </div>
-          <div style={contentStyle}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum. Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?
-          </div>
-          <div style={actionsStyle}>
-            <button
-              className="button"
-              onClick={() => {
-                toast.success('modal closed ');
-                console.log('modal closed ');
-                close();
-              }}
-            >
-              close modal
-            </button>
-          </div>
-        </div>
-      )}
-    </Popup>
-    </>
-  );
-};
-
+import OpenBets from '../views/ui/OpenBets.js'
+import LotteryMain from '../views/ui/LotteryMain.js'
 
 const Starter = () => {
   const { currentAccount } = useWeb3(); // Now using the useWeb3 hook
@@ -142,35 +28,8 @@ const Starter = () => {
 
   return (
     <div>
-      <PopupExample shortenAddress={currentAccount}/>
-      <Row>
-        <Col sm="6" lg="6" xl="7" xxl="8">
-          <SalesChart />
-        </Col>
-        <Col sm="6" lg="6" xl="5" xxl="4">
-          <Feeds />
-        </Col>
-      </Row>
-      {/***Table ***/}
-      <Row>
-        <Col lg="12">
-          <ProjectTables />
-        </Col>
-      </Row>
-      {/***Blog Cards***/}
-      <Row>
-        {BlogData.map((blg, index) => (
-          <Col sm="6" lg="6" xl="3" key={index}>
-            <Blog
-              image={blg.image}
-              title={blg.title}
-              subtitle={blg.subtitle}
-              text={blg.description}
-              color={blg.btnbg}
-            />
-          </Col>
-        ))}
-      </Row>
+      <OpenBets />
+      <LotteryMain />
     </div>
   );
 };
